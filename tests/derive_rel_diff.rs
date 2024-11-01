52e4d5876e657e90106526029ac449db6541897d
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-11-01 16:03:15 +0100

    add tests for RelativeEq

--- tests/derive_rel_diff.rs
+++ tests/derive_rel_diff.rs
@@ -147,3 +147,119 @@ fn derive_rel_diff_eq_equal_2() {
     };
     approx::assert_relative_ne!(p1, p2, max_relative = 0.1);
 }
+
+#[test]
+fn derive_abs_diff_eq_equal_2() {
+    #[derive(RelativeEq, PartialEq, Debug)]
+    #[approx(epsilon_type = f64)]
+    struct Prediction {
+        #[approx(equal)]
+        category: String,
+        confidence: f64,
+    }
+    let p1 = Prediction {
+        confidence: -1.0,
+        category: "my_horses".into(),
+    };
+    let p2 = Prediction {
+        confidence: -1.2,
+        category: "horses".into(),
+    };
+    approx::assert_abs_diff_ne!(p1, p2, epsilon = 0.3);
+}
+
+#[test]
+fn derive_abs_diff_option_2() {
+    #[derive(RelativeEq, PartialEq, Debug)]
+    struct Car {
+        max_speed: f32,
+        #[approx(map = |x| x)]
+        battery: Option<f32>,
+    }
+    let c1 = Car {
+        max_speed: 180.0,
+        battery: Some(1.0),
+    };
+    let c2 = Car {
+        max_speed: 180.1,
+        battery: Some(0.99),
+    };
+    let c3 = Car {
+        max_speed: 180.0,
+        battery: None,
+    };
+    let c4 = Car {
+        max_speed: 182.0,
+        battery: Some(1.1),
+    };
+    let c5 = Car {
+        max_speed: 177.0,
+        battery: Some(0.9),
+    };
+    approx::assert_abs_diff_eq!(c1, c2, epsilon = 0.15);
+    approx::assert_abs_diff_ne!(c1, c3, epsilon = 0.15);
+    approx::assert_abs_diff_ne!(c4, c3, epsilon = 10.0);
+    approx::assert_abs_diff_eq!(c4, c5, epsilon = 6.0);
+}
+
+#[test]
+fn derive_abs_diff_mapping() {
+    #[derive(RelativeEq, PartialEq, Debug)]
+    struct Cat {
+        weight: f32,
+        #[approx(map = |_| {Some(&0f32)})]
+        birthday: String,
+    }
+    let c1 = Cat {
+        weight: 5.3,
+        birthday: "19th of April 2022".into(),
+    };
+    let c2 = Cat {
+        weight: 5.3,
+        birthday: "19/04/2022".into(),
+    };
+    approx::assert_abs_diff_eq!(c1, c2);
+}
+
+#[test]
+fn derive_abs_diff_mapping_function() {
+    #[derive(PartialEq, Debug)]
+    enum Time {
+        Days(f64),
+        Weeks(f64),
+    }
+    fn time_to_days(t: &Time) -> Option<f64> {
+        match t {
+            Time::Days(d) => Some(*d),
+            Time::Weeks(w) => Some(7.0 * w),
+        }
+    }
+    #[derive(RelativeEq, PartialEq, Debug)]
+    struct Dogo {
+        age_in_weeks: f64,
+        #[approx(map = time_to_days)]
+        next_doctors_appointment: Time,
+    }
+    let d1 = Dogo {
+        age_in_weeks: 52.0,
+        next_doctors_appointment: Time::Days(35.0),
+    };
+    let d2 = Dogo {
+        age_in_weeks: 52.0,
+        next_doctors_appointment: Time::Weeks(5.0),
+    };
+    approx::assert_abs_diff_eq!(d1, d2, epsilon = 0.0);
+}
+
+#[test]
+fn derive_abs_diff_equal_higher_priority_than_mapping() {
+    #[derive(AbsDiffEq, PartialEq, Debug)]
+    struct Length {
+        #[approx(equal)]
+        #[approx(map = |x: &f32| Some(2.0*x))]
+        meters: f32,
+    }
+    let l1 = Length { meters: 3.0 };
+    let l2 = Length { meters: 3.0001 };
+    approx::assert_abs_diff_ne!(l1, l2, epsilon = 0.001);
+}
