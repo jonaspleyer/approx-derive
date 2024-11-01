f677337d39fdb9d06b3661883bee7826ecf4554b
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-11-01 09:55:03 +0100

    correc formatting

--- tests/derive_abs_diff.rs
+++ tests/derive_abs_diff.rs
@@ -148,9 +148,18 @@ fn derive_abs_diff_option() {
         #[approx(equal)]
         opt: Option<i32>,
     }
-    let c1 = ContainsOptional { value: 1.1, opt: Some(1) };
-    let c2 = ContainsOptional { value: 1.0, opt: Some(1) };
-    let c3 = ContainsOptional { value: 1.0, opt: Some(2) };
+    let c1 = ContainsOptional {
+        value: 1.1,
+        opt: Some(1),
+    };
+    let c2 = ContainsOptional {
+        value: 1.0,
+        opt: Some(1),
+    };
+    let c3 = ContainsOptional {
+        value: 1.0,
+        opt: Some(2),
+    };
     approx::assert_abs_diff_eq!(c1, c2, epsilon = 0.2);
     approx::assert_abs_diff_ne!(c2, c3);
 }
